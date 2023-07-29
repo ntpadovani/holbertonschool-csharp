c085be2c3ad28402bd7570bd279a2355e9ba2824
norman <normant_padovani@hotmail.com>
2023-07-28 23:53:07 -0400

    Hello world tasks

---
+++ 1-build_new_project.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+dotnet new console -n 1-new_project
+cd 1-new_project
+dotnet build
---
+++ 2-run_new_project.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+dotnet new console -n 2-new_project
+cd 2-new_project
+dotnet build
+dotnet run
